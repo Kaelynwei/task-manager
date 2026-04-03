89e97d48a256683e28cc70f6d142845cf1c1fa5c
Kaelyn <kaelynwei@gmail.com>
2026-04-03 17:40:32 +1100

    feat: implement Idea component

--- my-app/src/components/Idea.tsx
+++ my-app/src/components/Idea.tsx
@@ -3,45 +3,20 @@ interface Task{
     name: string;
 }
 
-function Idea() {
+interface IdeaProps{
+    task: Task;
+}
+
+function Idea({ task }: IdeaProps) {
     return (
-        <div className="container">
-            <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" id="check1"/>
-                <label className="form-check-label" htmlFor="check1">
-                    TASK1
-                </label>
-            </div>
-            <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" id="check2"/>
-                <label className="form-check-label" htmlFor="check2">
-                    TASK2
-                </label>
-                
-            </div>
+        <div className="form-check">
+            <input className="form-check-input" type="checkbox" id={`check-${task.id}`} />
+            <label className="form-check-label" htmlFor={`check-${task.id}`}>
+                {task.name}
+            </label>
         </div>
     );
 
 }
 
-
 export default Idea;
-
-/* get the value of an input element
-
-const input = document.getElementID('taskname') as HTMLInputElment | null;
-const value = input?.value;
-console.log(value)
-if (input != null) {
-    console.log(input.value);
-}
-
-input?.addEventListener('input', function (event) {
-    const target = event.target as HTMLInputElement;
-    console.log(target.value);
-});
-
-
-
-get value => map
-*/
