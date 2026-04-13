c13eac60383d8592144f7389ef9ae725deedd3b5
Chia-Yu Wei <156737860+Kaelynwei@users.noreply.github.com>
2026-04-13 17:41:13 +1000

    Feature/klp4 complete task (#4)
    * feat: initialize React app and update README file
    
    * feat: implement Idea component
    
    * feat: display static IdeaList
    
    * feat: implement delete task

--- my-app/src/components/Idea.tsx
+++ my-app/src/components/Idea.tsx
@@ -1,22 +1,38 @@
-interface Task{
-    id: number;
-    name: string;
-}
+import { type Task } from '../Type';
 
 interface IdeaProps{
     task: Task;
+    onToggleStatus:(id: number) =>void;
+    onDelete:(id: number) =>void;
 }
 
-function Idea({ task }: IdeaProps) {
+function Idea({ task, onToggleStatus, onDelete }: IdeaProps) {
     return (
-        <div className="form-check">
-            <input className="form-check-input" type="checkbox" id={`check-${task.id}`} />
-            <label className="form-check-label" htmlFor={`check-${task.id}`}>
-                {task.name}
-            </label>
+        <div className="form-check d-flex justify-content-between align-items-center mb-2">
+            <div>
+                <input 
+                    className="form-check-input" 
+                    type="checkbox" 
+                    id={`check-${task.id}`} 
+                    onChange={() => onToggleStatus(task.id)} 
+                    checked={task.completed}
+                />
+                <label 
+                    className={`form-check-label ${task.completed ? 'text-decoration-line-through text-muted' : ''}`} 
+                    htmlFor={`check-${task.id}`}
+                >
+                    {task.name}
+                </label>
+            </div>
+            <button
+                className="btn btn-outline-danger btn-sm"
+                onClick={() => onDelete(task.id)}
+            >
+                Delete
+            </button>
         </div>
     );
-
 }
+
 
 export default Idea;
--- my-app/src/components/IdeaList.tsx
+++ my-app/src/components/IdeaList.tsx
@@ -1,28 +1,38 @@
 import Idea from "./Idea";
-
-interface Task {
-    id: number;
-    name: string;
-
-}
+import { type Task } from '../Type';
+import { useState } from 'react';
 
 function IdeaList() {
-    let myIdeas: Task[] = [
-        {id: 1, name: "first task"},
-        {id: 2, name: "second task"},
-        {id: 3, name: "third task"}
+    const [tasks, setTasks] = useState<Task[]> ([
+        {id: 1, name: "first task", completed: false},
+        {id: 2, name: "second task", completed: false},
+        {id: 3, name: "third task", completed: false}
 
-    ];
+    ]);
+    const handleToggle = (id:number) =>{
+        const updateTasks = tasks.map(task=> {
+            if (task.id === id){
+                return {...task, completed: !task.completed};
+            }
+            return task;
+        });
+        setTasks(updateTasks);
+    };
+
+    const handleDelete = (id:number) =>{
+        setTasks(tasks.filter(task => task.id != id))
+    };
+
+    const sortedTasks = [...tasks].sort((a,b)=> Number(a.completed)-Number(b.completed));
 
     return (
-        <div className="container">
+        <div className="container" >
             <hr />
-            {myIdeas.map((item) => (
-                <Idea key = {item.id} task = {item} />
+            {sortedTasks.map((item) => (
+                <Idea key={item.id} task={item} onToggleStatus={handleToggle} onDelete={handleDelete}/>
             ))}
         </div>
     );
-
 }
 
 export default IdeaList;
